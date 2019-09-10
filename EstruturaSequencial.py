5ba18ec313edfdecc01257b4f0416a459daf5f00
danusarbc <danusarbc@gmail.com>
2019-09-10 17:05:01 -0300

    Sequencial Structure Exercise

---
+++ EstruturaSequencial.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#EstruturaSequencial
+
+# 1. Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+print("Hello World")
+
+# 2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+num = input("Type a number: ")
+print("Your numer is: ", num)
+
+# 3. Faça um Programa que peça dois números e imprima a soma.
+numA = input("Type a first number:")
+numB = input("Type a second number: ")
+result = int(numA) + int(numB)
+print("Result: ", result)
+
+
+
