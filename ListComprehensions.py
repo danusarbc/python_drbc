58c892afdc2f60c17a4bd02baf4791c88e47a1fe
danusarbc <danusarbc@gmail.com>
2019-09-12 13:42:37 -0300

    List Comprehensions exerrcise

---
+++ ListComprehensions.py
@@ -0,0 +1,12 @@
+# List Comprehensions
+# Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. 
+# Write a code in Python that takes this list a and makes a new list that has only the even elements of this list in it.
+
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+evenList = []
+
+for i in a:
+    if i % 2 == 0:
+        evenList.append(i)
+print("Even elements: ", evenList)
