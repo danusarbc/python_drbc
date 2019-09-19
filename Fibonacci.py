73e48e6ec4278cdec8976ee00014af44b9ca7647
danusarbc <danusarbc@gmail.com>
2019-09-19 13:53:21 -0300

    Fibonacci exercise

---
+++ Fibonacci.py
@@ -0,0 +1,13 @@
+#Fibonacci
+def Fibonacci(num):
+
+    if num <=1:
+        return num
+    else:
+        return Fibonacci(num -1) + Fibonacci(num - 2)
+
+fib = int(input("Enter a number: "))
+
+result = Fibonacci(fib)
+
+print ("Fibonacci number:", result)
